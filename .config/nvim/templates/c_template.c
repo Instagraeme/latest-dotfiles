a2a7a51ca5b915f717ddd9359f55640f672f5d61
Graeme Smith <gs78@me.com>
2022-06-14 20:51:38 +0100

    Added neovim config

---
+++ .config/nvim/templates/c_template.c
@@ -0,0 +1,46 @@
+/**************************************************
+ *
+ * File Name     : c_template.c
+ * Author        : Graeme Smith
+ * Purpose       :
+ * Version       : 0.0.1
+ * Creation Date : 27.03.2020
+ * Last Modified : Tue 14 Jun 20:50:02 2022
+ *
+ **************************************************/
+
+// SPDX-License-Identifier: CC-BY-NC-ND-3.0
+// Copyright (c) %%YEAR%% Graeme Smith. All rights reserved.
+
+
+/* 2. Included Libraries */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <errno.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <time.h>
+
+/* 3. Macro Definitions */
+
+/* 4. Type Definitions */
+
+/* 5. Global Variables */
+
+/* 6. Function Prototypes */
+
+
+/**************************************************
+ *
+ * Main Function
+ *
+ **************************************************/
+
+int main(int argc, char *argv[]) {
+   
+  return EXIT_SUCCESS;
+}
+
